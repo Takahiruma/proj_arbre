193b7c82e7a9abb830412b0dfb44c9f3855a3af1
Takahiruma <geoffreydan@wanadoo.fr>
2018-01-09 16:56:31 +0100

    euh je ne sais pas quoi mettre

--- arbre_de_geoffrey/arbre.cpp
+++ arbre_de_geoffrey/arbre.cpp
@@ -8,20 +8,27 @@ arbre::~arbre(){
 	delete root;
 }
 noeud arbre::trouve_noeud(std::string s, noeud* n){
-	noeud* ad= new noeud (s,nullptr);
-	ad=nullptr;
+	noeud* ad= nullptr;
 	
+	if(n->get_mot() == s)
+		ad=n;
+	else if (n->get_mot() < s)
+		ad=trouve_noeud(s,n->get_gauche());
+	else
+		ad=trouve_noeud(s,n->get_droite());
 	
 	
 	return *ad;
 }
 
-// void arbre::inserer_noeud(noeud* n){
-	// if (isEmpty(root))
-		// root= new noeud(n);
-	// else
-		// if(n->get_mot() < root->get_mot()){
-			// if(n->
-		// }
+void arbre::inserer_noeud(noeud* n){
+	noeud *here = &root;
+	if (here->get_mot() == n->get_mot())
+		root= new noeud(n);
+	else
+		if(n->get_mot() < root->get_mot()){
+			if(root->get_gauche() == IsEmpty())
+				
+		}
 			
-// }
+}
