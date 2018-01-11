b9a2b5042a5fb6bef9841b1dd020da3f9ff54f20
Takahiruma <geoffreydan@wanadoo.fr>
2018-01-11 21:14:33 +0100

    AAAAAAAAH

--- arbre_de_geoffrey/main.cpp
+++ arbre_de_geoffrey/main.cpp
@@ -8,12 +8,14 @@ int main(int argc, char* argv[])
 	try
 	{
 		struct param p = eval_param(argc,argv);
+		string_map sm;
 		for (int i = 0; i < p.length;i++)
 		{
 			switch(p.copt[i])
 			{
 				case 'c':
-					// string_map sm = read_file(p.value[i]);
+					sm = read_file(p.value[i]);
+					//cout << sm["e"];
 					break;
 				case 'o':
 					cout <<"a";
